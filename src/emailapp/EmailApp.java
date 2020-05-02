76bb6694cb3bab24359178e750a825c1bedd6d9c
Weiquan Huang <47123863+weiquanh@users.noreply.github.com>
2020-05-01 23:38:12 -0700

    Update EmailApp.java

--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -4,12 +4,12 @@ package emailapp;
 // charged with the task of creating email accounts for new hires.
 // Email application should do the following:
 // 1. Generate an email with the following syntax: 
-//	  firstname.lastname@department.company.com
+//    firstname.lastname@department.company.com
 // 2. Determine the department (sales, development, accounting), 
-//	  if none leave blank
+//    if none leave blank
 // 3. Generate a random String for a password
 // 4. Have set methods to change the password, set the mailbox capacity, 
-//	  and define an alternate email address
+//    and define an alternate email address
 // 5. Have get methods to display the name, email, and mailbox capacity
 
 public class EmailApp {
